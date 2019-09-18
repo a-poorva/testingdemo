05d0601a44a623cbe211eda1c9c719d3946a3190
Apoorva Ramamurthy <apoorvaramamurthy@Apoorvas-MacBook-Air-785.local>
2019-09-18 12:18:45 -0400

    Adding subtraction file and tetsing file

---
+++ minus.py
@@ -0,0 +1,6 @@
+def subtract(a, b):
+    return a - b
+
+a = 4
+b = 5
+print(subtract(a,b))
---
+++ test_minus.py
@@ -0,0 +1,14 @@
+import pytest
+
+@pytest.mark.parametrize("a, b, expected", [
+(4,2,2),
+(7,3,4),
+(8,5,3),
+(10,2,8),
+])
+
+
+def test_minus(a,b, expected):
+    from minus import subtract
+    result = subtract(a,b)
+    assert result == pytest.approx(expected)
